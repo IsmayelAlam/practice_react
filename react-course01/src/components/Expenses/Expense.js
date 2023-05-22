d8d51dc645d90c1c2f5288ea96fb125c21a98e38
IsmayelAlam <ismayelalam0@gmail.com>
2023-05-22 11:59:17 +0600

    add chart

--- react-course01/src/components/Expenses/Expense.js
+++ react-course01/src/components/Expenses/Expense.js
@@ -1,41 +1,26 @@
-import ExpenseItem from "./ExpenseItem";
 import "./css/Expense.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
 import { useState } from "react";
+import ExpensesList from "./ExpenseList";
+import ExpensesChart from "./ExpenseChart";
 
 function Expense(props) {
-  const expense = props.expense;
-
   const [filterYear, setFilterYear] = useState("2020");
 
   function filterChange(selectedYear) {
     setFilterYear(selectedYear);
   }
 
+  const filteredExpenses = props.expense.filter((expense) => {
+    return expense.date.getFullYear().toString() === filterYear;
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filterYear} onChangeFilter={filterChange} />
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      />
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      />
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      />
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      />
+      <ExpensesChart expenses={filteredExpenses} />
+      <ExpensesList items={filteredExpenses} />
     </Card>
   );
 }
