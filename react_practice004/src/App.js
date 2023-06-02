31eaee8efd4f3b8a8c267d27d731b7e3a3f0f311
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-02 21:56:06 +0600

    finalize the app

--- react_practice004/src/App.js
+++ react_practice004/src/App.js
@@ -1,16 +1,49 @@
+import "bulma/css/bulma.css";
+
 import ProfileCard from "./ProfileCard";
 import AlaxaImg from "./Assets/alexa.png";
 import CortanaImg from "./Assets/cortana.png";
-import SiriImg from "../Assets/siri.png";
+import SiriImg from "./Assets/siri.png";
 
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
 
-      <ProfileCard title="Alexa" handle="@alexa789" />
-      <ProfileCard title="Cortana" handle="@cortana456" />
-      <ProfileCard title="Siri" handle="@siri123" />
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa789"
+                image={AlaxaImg}
+                description="Lorem ipsum dolor sit amet consectetur, adipisicing."
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana"
+                image={CortanaImg}
+                description="Adipisci omnis nisi cum fugit enim voluptatibus voluptatibus."
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Siri"
+                handle="@siri123"
+                image={SiriImg}
+                description="Facilis asperiores repellat accusamus saepe."
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
