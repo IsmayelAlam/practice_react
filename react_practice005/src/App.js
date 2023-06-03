22f7ba8afd5c5c7aa1ce28d3723ac62b1551dc76
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-03 12:42:36 +0600

    add images

--- react_practice005/src/App.js
+++ react_practice005/src/App.js
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
-  return <div>Show animal list here</div>;
+  const [animals, setAnimal] = useState([]);
+
+  const handleClick = () => {
+    setAnimal([...animals, getRandomAnimal()]);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+      {animals.map((animal, index) => (
+        <AnimalShow type={animal} key={index} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
