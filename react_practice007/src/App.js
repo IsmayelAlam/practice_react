24602c3e96cb3997c82458c85530c15fefb2c7d5
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-05 20:36:57 +0600

    add context file to the app

--- react_practice007/src/App.js
+++ react_practice007/src/App.js
@@ -1,53 +1,21 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
+
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
-import axios from "axios";
+import BooksContext from "./context/books";
 
 function App() {
-  const [books, setBooks] = useState([]);
-
-  const fetchBooks = async () => {
-    const response = await axios.get("http://localhost:3001/books");
-    setBooks(response.data);
-  };
+  const { fetchBooks } = useContext(BooksContext);
 
   useEffect(() => {
     fetchBooks();
   }, []);
 
-  const createBook = async (title) => {
-    const response = await axios.post("http://localhost:3001/books", {
-      title,
-    });
-
-    const newBooks = [...books, response.data];
-
-    setBooks(newBooks);
-  };
-
-  const deleteBook = async (id) => {
-    await axios.delete(`http://localhost:3001/books/${id}`);
-
-    setBooks(books.filter((book) => book.id !== id));
-  };
-
-  const editBook = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle,
-    });
-
-    setBooks(
-      books.map((book) =>
-        book.id === id ? { ...book, ...response.data } : book
-      )
-    );
-  };
-
   return (
     <div className="app">
       <h1>Reading List</h1>
-      <BookList books={books} onDelete={deleteBook} onEdit={editBook} />
-      <BookCreate onCreate={createBook} />
+      <BookList />
+      <BookCreate />
     </div>
   );
 }
---
+++ react_practice007/src/context/books.js
@@ -0,0 +1,46 @@
+import { createContext, useState } from "react";
+import axios from "axios";
+
+const BooksContext = createContext();
+
+function Provider({ children }) {
+  const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
+  };
+
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    const newBooks = [...books, response.data];
+
+    setBooks(newBooks);
+  };
+
+  const deleteBook = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
+    setBooks(books.filter((book) => book.id !== id));
+  };
+
+  const editBook = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
+    setBooks(
+      books.map((book) =>
+        book.id === id ? { ...book, ...response.data } : book
+      )
+    );
+  };
+
+  return <BooksContext.Provider value={{}}>{children}</BooksContext.Provider>;
+}
+
+export default BooksContext;
+export { Provider };
