f1c8ad049fdc5b942efe7df92339eec506630210
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-06 11:32:46 +0600

    add tailwind to the project

--- react_practice008/src/App.js
+++ react_practice008/src/App.js
@@ -1,5 +1,15 @@
+import Button from "./Button";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button className="block	">Button 01</Button>
+      <Button>Button 02</Button>
+      <Button>Button 03</Button>
+      <Button>Button 04</Button>
+      <Button>Button 05</Button>
+    </div>
+  );
 }
 
 export default App;
