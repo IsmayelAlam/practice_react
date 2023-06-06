1284c65f56ba514535b5934829f9839cb68430f3
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-06 13:00:09 +0600

    handle events on the button

--- react_practice008/src/App.js
+++ react_practice008/src/App.js
@@ -2,16 +2,26 @@ import Button from "./Button";
 import { GoBell, GoAlert, GoCheck } from "react-icons/go";
 
 function App() {
+  const handleClick = () => console.log("click");
+  const handleMove = () => console.log("move");
+  const handleLeave = () => console.log("leave");
+
   return (
     <div className="bg-slate-700 h-screen ">
-      <Button outline primary>
+      <Button outline primary onClick={handleClick}>
         <GoBell />
         Button 01
       </Button>
-      <Button outline secondary>
+      <Button outline secondary onMouseMove={handleMove}>
         Button 02
       </Button>
-      <Button rounded outline success>
+      <Button
+        rounded
+        outline
+        success
+        className="mb-5"
+        onMouseLeave={handleLeave}
+      >
         <GoCheck />
         Button 03
       </Button>
--- react_practice008/src/Button.js
+++ react_practice008/src/Button.js
@@ -1,4 +1,4 @@
-import className from "classnames";
+import classNames from "classnames";
 
 function Button({
   children,
@@ -9,23 +9,33 @@ function Button({
   danger,
   outline,
   rounded,
+  ...rest
 }) {
-  const classes = className("px-3 py-1.5 m-5 border", {
-    "bg-blue-500 border-blue-600 text-white": primary,
-    "bg-gray-500 border-gray-600 text-white": secondary,
-    "bg-green-500 border-green-600 text-white": success,
-    "bg-yellow-500 border-yellow-600 text-white": warning,
-    "bg-red-500 border-red-600 text-white": danger,
-    "rounded-3xl": rounded,
-    " bg-white ": outline,
-    "text-blue-500": outline && primary,
-    "text-gray-900 ": outline && secondary,
-    "text-green-500": outline && success,
-    "text-yellow-400": outline && warning,
-    "text-red-500": outline && danger,
-  });
+  const classes = classNames(
+    `px-3 py-1.5 m-5 border flex items-center ${rest.className}`,
+    {
+      "bg-blue-500 border-blue-600 text-white": primary,
+      "bg-gray-500 border-gray-600 text-white": secondary,
+      "bg-green-500 border-green-600 text-white": success,
+      "bg-yellow-500 border-yellow-600 text-white": warning,
+      "bg-red-500 border-red-600 text-white": danger,
+      "rounded-3xl": rounded,
+      " bg-white ": outline,
+      "text-blue-500": outline && primary,
+      "text-gray-900 ": outline && secondary,
+      "text-green-500": outline && success,
+      "text-yellow-400": outline && warning,
+      "text-red-500": outline && danger,
+    }
+  );
 
-  return <button className={classes}>{children}</button>;
+  console.log(classes);
+
+  return (
+    <button className={classes} {...rest}>
+      {children}
+    </button>
+  );
 }
 
 Button.propTypes = {
