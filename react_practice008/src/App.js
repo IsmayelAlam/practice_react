8ef71add3b29916124068d7bf9f8beb63ae56140
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-06 12:32:45 +0600

    add icons

--- react_practice008/src/App.js
+++ react_practice008/src/App.js
@@ -1,21 +1,25 @@
 import Button from "./Button";
+import { GoBell, GoAlert, GoCheck } from "react-icons/go";
 
 function App() {
   return (
-    <div className="bg-slate-700 h-screen text-center">
+    <div className="bg-slate-700 h-screen ">
       <Button outline primary>
+        <GoBell />
         Button 01
       </Button>
       <Button outline secondary>
         Button 02
       </Button>
       <Button rounded outline success>
+        <GoCheck />
         Button 03
       </Button>
       <Button rounded outline warning>
         Button 04
       </Button>
       <Button rounded outline danger>
+        <GoAlert />
         Button 05
       </Button>
     </div>
