e1156fd9b5fee68a74fe4b36fe6bd6cd33a290fc
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-07 21:06:48 +0600

    style modal component

--- react_practice008/src/components/Modal.js
+++ react_practice008/src/components/Modal.js
@@ -1,5 +1,25 @@
-function Modal() {
-  return <div>Modal</div>;
+import ReactDom from "react-dom";
+import { useEffect } from "react";
+
+function Modal({ onClose, children }) {
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+    return () => document.body.classList.remove("overflow-hidden");
+  }, []);
+  return ReactDom.createPortal(
+    <div>
+      <div
+        onClick={onClose}
+        className="fixed inset-0 bg-gray-300 opacity-80"
+      ></div>
+      <div className="fixed inset-40 p-10 bg-white ">
+        <div className="flex flex-col justify-between items-end h-full">
+          {children}
+        </div>
+      </div>
+    </div>,
+    document.querySelector(".modal")
+  );
 }
 
 export default Modal;
--- react_practice008/src/pages/ModalPage.js
+++ react_practice008/src/pages/ModalPage.js
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import Button from "../components/Button";
 import Modal from "../components/Modal";
 
 function ModalPage() {
-  return <Modal />;
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => setShowModal(!showModal);
+
+  return (
+    <div className="relative">
+      <Button onClick={handleClick} primary rounded>
+        Modal
+      </Button>
+      {showModal && (
+        <Modal onClose={handleClick}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+            expedita amet cum exercitationem ad aut ut odio eligendi fugiat
+            veniam. Ex fugit quo nisi nemo debitis deleniti porro dicta eos?
+          </p>
+          <Button primary onClick={handleClick}>
+            close
+          </Button>
+        </Modal>
+      )}
+    </div>
+  );
 }
 
 export default ModalPage;
