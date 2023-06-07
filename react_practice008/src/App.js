ce6b8cbb211016aa1ba127a3c4bb4448de43a22e
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-07 11:59:22 +0600

    add functionality to the dropdown component

--- react_practice008/src/App.js
+++ react_practice008/src/App.js
@@ -1,12 +1,25 @@
 // import ButtonPage from "./pages/ButtonPage";
 // import AccordionPage from "./pages/AccordionPage";
 
+import { useState } from "react";
 import DropDown from "./components/DropDown";
 
 function App() {
+  const [selected, setSelected] = useState(null);
+
+  const options = [
+    { label: "Red", value: "red" },
+    { label: "Green", value: "green" },
+    { label: "Blue", value: "blue" },
+  ];
+
+  const handleSelect = (option) => {
+    setSelected(option);
+  };
+
   return (
     <div className="bg-slate-700 h-screen">
-      <DropDown />
+      <DropDown options={options} onChange={handleSelect} value={selected} />
     </div>
   );
 }
--- react_practice008/src/components/DropDown.js
+++ react_practice008/src/components/DropDown.js
@@ -1,5 +1,25 @@
-function DropDown() {
-  return <div>DropDwon</div>;
+import { useState } from "react";
+
+function DropDown({ options, onChange, value }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOption = (value) => {
+    onChange(value);
+    setIsOpen(false);
+  };
+
+  const renderOptions = options.map((option) => (
+    <div onClick={handleOption.bind(null, option.label)} key={option.value}>
+      {option.label}
+    </div>
+  ));
+
+  return (
+    <div>
+      <div onClick={() => setIsOpen(!isOpen)}>{value || "Select..."}</div>
+      {isOpen && <div>{renderOptions}</div>}
+    </div>
+  );
 }
 
 export default DropDown;
