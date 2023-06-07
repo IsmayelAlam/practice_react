e1ca95e7614fb8fea59f0b6f6f63f74ee173dd3d
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-07 10:51:16 +0600

    add new component: drop down

--- react_practice008/src/App.js
+++ react_practice008/src/App.js
@@ -1,8 +1,14 @@
 // import ButtonPage from "./pages/ButtonPage";
 // import AccordionPage from "./pages/AccordionPage";
 
+import DropDown from "./components/DropDown";
+
 function App() {
-  return <div className="bg-slate-700 h-screen"></div>;
+  return (
+    <div className="bg-slate-700 h-screen">
+      <DropDown />
+    </div>
+  );
 }
 
 export default App;
---
+++ react_practice008/src/components/DropDown.js
@@ -0,0 +1,5 @@
+function DropDown() {
+  return <div>DropDwon</div>;
+}
+
+export default DropDown;
