cd4ed3249bde3a3ecbfdac30a14cfcf63f9a10f8
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-07 15:52:53 +0600

    add seperate page for dropdown component

--- react_practice008/src/App.js
+++ react_practice008/src/App.js
@@ -1,26 +1,11 @@
 // import ButtonPage from "./pages/ButtonPage";
 // import AccordionPage from "./pages/AccordionPage";
-import DropDown from "./components/DropDown";
-
-import { useState } from "react";
+import DropDownPage from "./pages/DropDownPage";
 
 function App() {
-  const [selected, setSelected] = useState(null);
-
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" },
-  ];
-
-  const handleSelect = (option) => {
-    setSelected(option);
-  };
-
   return (
     <div className="bg-slate-700 h-screen flex">
-      <DropDown options={options} onChange={handleSelect} value={selected} />
-      <DropDown options={options} onChange={handleSelect} value={selected} />
+      <DropDownPage />
     </div>
   );
 }
