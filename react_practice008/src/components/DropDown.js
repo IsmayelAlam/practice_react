edb89dd599559d1fb1a5f3a5c90a9f5f3c01d036
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-07 12:40:57 +0600

    add style to dropdown component

--- react_practice008/src/components/DropDown.js
+++ react_practice008/src/components/DropDown.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
+import className from "classnames";
 
 function DropDown({ options, onChange, value }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,16 +10,32 @@ function DropDown({ options, onChange, value }) {
     setIsOpen(false);
   };
 
+  const classes = className(
+    "border rounded shadow cursor-pointer bg-white p-3 w-full"
+  );
+
   const renderOptions = options.map((option) => (
-    <div onClick={handleOption.bind(null, option.label)} key={option.value}>
+    <div
+      className="hover:bg-sky-100 rounded cursor-pointer p-1"
+      onClick={handleOption.bind(null, option.label)}
+      key={option.value}
+    >
       {option.label}
     </div>
   ));
 
   return (
-    <div>
-      <div onClick={() => setIsOpen(!isOpen)}>{value || "Select..."}</div>
-      {isOpen && <div>{renderOptions}</div>}
+    <div className="w-48 relative">
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className={"flex justify-between items-center " + classes}
+      >
+        {value || "Select..."}
+        <GoChevronDown />
+      </div>
+      {isOpen && (
+        <div className={"absolute top-full" + classes}>{renderOptions}</div>
+      )}
     </div>
   );
 }
