ffc862082029d53f9d77228ae44a54a3f11d3f23
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-08 16:38:04 +0600

    add functionality to the sortableTable component

--- react_practice008/src/components/SortableTable.js
+++ react_practice008/src/components/SortableTable.js
@@ -1,7 +1,74 @@
+import { useState } from "react";
 import Table from "./Table";
+import {
+  HiBarsArrowDown,
+  HiBarsArrowUp,
+  HiArrowsUpDown,
+} from "react-icons/hi2";
 
 function SortableTable(props) {
-  return <Table {...props} />;
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+
+  const { config, data } = props;
+
+  const handleClick = (label) => {
+    switch (sortOrder) {
+      case null:
+        setSortOrder("asc");
+        setSortBy(label);
+        break;
+      case "asc":
+        setSortOrder("desc");
+        setSortBy(label);
+        break;
+      case "desc":
+        setSortOrder(null);
+        break;
+      default:
+        return;
+    }
+  };
+
+  let sortedData = data;
+
+  if (sortBy && sortOrder) {
+    const { sortValue } = config.find((row) => row.label === sortBy);
+
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+
+      const reverseOder = sortOrder === "asc" ? 1 : -1;
+
+      if (typeof valueA === "string")
+        return valueA.localeCompare(valueB) * reverseOder;
+
+      return (valueA - valueB) * reverseOder;
+    });
+  }
+
+  const updateConfig = config.map((row) => {
+    if (!row.sortValue) return row;
+
+    return {
+      ...row,
+      header: () => (
+        <th onClick={handleClick.bind(null, row.label)}>
+          {getIcon(row.label, sortBy, sortOrder)}
+          {row.label}
+        </th>
+      ),
+    };
+  });
+
+  return <Table {...props} data={sortedData} config={updateConfig} />;
+}
+
+function getIcon(label, sortBy, sortOrder) {
+  if (label !== sortBy || sortOrder === null) return <HiArrowsUpDown />;
+  if (sortOrder === "asc") return <HiBarsArrowUp />;
+  if (sortOrder === "desc") return <HiBarsArrowDown />;
 }
 
 export default SortableTable;
