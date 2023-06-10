019e954c2f6ee17d485e03aca5d2f7c50f9eefbd
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-10 10:12:36 +0600

    add new project

---
+++ react_practice009/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <p className="title">Personal Digital Assistants</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ react_practice009/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const Element = document.getElementById("root");
+
+const root = ReactDOM.createRoot(Element);
+
+root.render(<App />);
