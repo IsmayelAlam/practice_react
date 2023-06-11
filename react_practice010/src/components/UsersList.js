3d533249264e93d23c93b4decbf5b28e991d9e17
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-11 21:29:43 +0600

    fetch data with async thunks

--- react_practice010/src/components/UsersList.js
+++ react_practice010/src/components/UsersList.js
@@ -1,3 +1,14 @@
+import { useEffect, useReducer } from "react";
+import { fetchUsers } from "../store";
+import { useDispatch } from "react-redux";
+
 export default function UsersList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+    console.log("test");
+  }, [dispatch]);
+
   return <div>UsersList</div>;
 }
