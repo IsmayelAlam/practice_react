f7701056745ddfa57ecc71ff4afaba01f4641b98
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-11 13:11:58 +0600

    add basic style

--- react_practice009/src/App.js
+++ react_practice009/src/App.js
@@ -5,11 +5,11 @@ import CarValue from "./Components/CarValue";
 
 function App() {
   return (
-    <div>
+    <div className="container is-fluid">
       <CarForm />
+      <CarSearch />
       <CarList />
       <CarValue />
-      <CarSearch />
     </div>
   );
 }
