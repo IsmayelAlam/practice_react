d9392ee0cac8fb73367c3ee77ee67b21843d0960
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-11 10:57:04 +0600

    add redux componentes to manage state

--- react_practice009/src/App.js
+++ react_practice009/src/App.js
@@ -1,5 +1,17 @@
+import CarForm from "./Components/CarForm";
+import CarList from "./Components/CarList";
+import CarSearch from "./Components/CarSearch";
+import CarValue from "./Components/CarValue";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <div>
+      <CarForm />
+      <CarList />
+      <CarValue />
+      <CarSearch />
+    </div>
+  );
 }
 
 export default App;
