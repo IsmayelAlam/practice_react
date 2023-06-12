1c7415c36374ac498c52eab40b6063524b4fda3f
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-12 11:01:08 +0600

    add new users automatically

--- react_practice010/src/components/UsersList.js
+++ react_practice010/src/components/UsersList.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
-import { fetchUsers } from "../store";
+import { addUser, fetchUsers } from "../store";
+import Button from "./Button";
 import { useDispatch, useSelector } from "react-redux";
 import Skeleton from "./Skeleton";
 
@@ -10,20 +11,39 @@ export default function UsersList() {
 
   useEffect(() => {
     dispatch(fetchUsers());
-    console.log("test");
   }, [dispatch]);
 
-  if (isLoading)
+  const handleUserAdd = () => dispatch(addUser());
+
+  if (isLoading) {
     return (
       <div>
-        <Skeleton className={"h-5 w-full"} />
-        <Skeleton className={"h-5 w-5"} />
-        <Skeleton className={"h-5 w-10"} />
-        <Skeleton className={"h-10 w-full"} />
-        <Skeleton className={"h-10 w-10"} />
+        <Skeleton className={"h-10 w-full"} times={data.length} />
       </div>
     );
+  }
+
   if (error) return <div>Error fetching data...</div>;
 
-  return <div>{data.length}</div>;
+  const renderUser = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        <Button rounded secondary onClick={handleUserAdd}>
+          + Add Users {data.length}
+        </Button>
+      </div>
+      {renderUser}
+    </div>
+  );
 }
