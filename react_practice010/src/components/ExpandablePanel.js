6774b2d13f4276fd95b95aac10419ec393ff44a0
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-12 23:07:44 +0600

    add expandable function

--- react_practice010/src/components/ExpandablePanel.js
+++ react_practice010/src/components/ExpandablePanel.js
@@ -1,3 +1,22 @@
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
+
 export default function ExpandablePanel({ header, children }) {
-  return <div>hi</div>;
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpand = () => setExpanded(!expanded);
+
+  return (
+    <div className="mb-2 rounded bg-slate-700 text-white">
+      <div className="flex p-2 justify-between items-center">
+        <div className="flex flex-row items-center justify-between gap-3">
+          {header}
+        </div>
+        <div onClick={handleExpand} className="cursor-pointer">
+          {expanded ? <GoChevronDown /> : <GoChevronLeft />}
+        </div>
+      </div>
+      {expanded && <div className="p-2 border-t">children</div>}
+    </div>
+  );
 }
--- react_practice010/src/components/UserListItems.js
+++ react_practice010/src/components/UserListItems.js
@@ -2,6 +2,7 @@ import Button from "./Button";
 import { GoTrashcan } from "react-icons/go";
 import { removeUser } from "../store";
 import useThunk from "../hooks/useThunk";
+import ExpandablePanel from "./ExpandablePanel";
 
 export default function UsersListItem({ user }) {
   const [doRemoveUser, isLoading] = useThunk(removeUser);
@@ -10,16 +11,14 @@ export default function UsersListItem({ user }) {
     doRemoveUser(user);
   };
 
-  return (
-    <div className="mb-2 rounded bg-slate-700 text-white">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between gap-3">
-          <Button rounded secondary onClick={handleDelete} loading={isLoading}>
-            <GoTrashcan />
-          </Button>
-          {user.name}
-        </div>
-      </div>
-    </div>
+  const header = (
+    <>
+      <Button rounded secondary onClick={handleDelete} loading={isLoading}>
+        <GoTrashcan />
+      </Button>
+      {user.name}
+    </>
   );
+
+  return <ExpandablePanel header={header}>content</ExpandablePanel>;
 }
