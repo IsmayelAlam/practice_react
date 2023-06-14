8385a24c58eae7a563cb946b98fb95361577a45c
IsmayelAlam <ismayelalam0@gmail.com>
2023-06-14 19:46:23 +0600

    add content and style to addExpense component

--- expense_tracker/src/Components/AddExpense.js
+++ expense_tracker/src/Components/AddExpense.js
@@ -1,3 +1,41 @@
 export default function AddExpense() {
-  return <div className=" bg-green-500 col-start-2 col-end-6">AddExpense</div>;
+  return (
+    <div className="col-start-2 col-end-6 flex items-center">
+      <form className="w-full flex gap-10 items-end px-10">
+        <div className="flex flex-col text-lg w-1/5 uppercase">
+          <label htmlFor="name">description</label>
+          <input
+            type="text"
+            id="name"
+            required
+            className="rounded p-2 border-b-2 border-slate-500 bg-slate-300 valid:border-green-400 shadow"
+          />
+        </div>
+        <div className="flex flex-col text-lg w-1/5 uppercase">
+          <label htmlFor="amount">amount</label>
+          <input
+            type="number"
+            id="amount"
+            required
+            className="rounded p-2 border-b-2 border-slate-500 bg-slate-300 valid:border-green-400 shadow"
+          />
+        </div>
+        <div className="flex flex-col text-lg w-1/5 uppercase">
+          <label htmlFor="date">date</label>
+          <input
+            type="date"
+            id="date"
+            required
+            className="rounded p-2 border-b-2 border-slate-500 bg-slate-300 valid:border-green-400 shadow"
+          />
+        </div>
+        <button
+          type="submit"
+          className="px-10 py-3 bg-slate-700 text-white rounded"
+        >
+          Add
+        </button>
+      </form>
+    </div>
+  );
 }
--- expense_tracker/src/Components/SideBar.js
+++ expense_tracker/src/Components/SideBar.js
@@ -1,21 +1,23 @@
 export default function SideBar() {
   return (
-    <div className="row-span-full p-10 select-none flex flex-col">
+    <div className="row-span-full p-10 select-none flex flex-col border-r-2 border-slate-600 shadow">
       <h1 className="text-center text-3xl uppercase p-5 font-semibold">
         Expense tracker
       </h1>
-      <hr />
-      <div className="my-20">
+      <hr className="border border-stone-600" />
+      <div className="mt-20">
         <h2 className="font-bold">Your Balance</h2>
-        <p className="text-4xl m-2">$ 750</p>
+        <p className="text-5xl m-2">$ 750</p>
       </div>
       <div className="my-20">
         <h2 className="font-bold text-center text-xl">Cash Flow</h2>
-        <div className="p-10 border-4 border-green-600 rounded-md uppercase my-5 text-green-600 text-lg shadow-md">
-          income - $1950
+        <div className="p-10 border-2 border-green-600 rounded-md uppercase my-5 text-green-600 font-bold shadow-md text-center">
+          <p>Income</p>
+          <p className="text-3xl font-normal">$1950</p>
         </div>
-        <div className="p-10 border-4 border-red-600 rounded-md uppercase my-5 text-red-600 text-lg shadow-md">
-          Expense - $1250
+        <div className="p-10 border-2 border-red-600 rounded-md uppercase my-5 text-red-600 font-bold shadow-md text-center">
+          <p>Expense</p>
+          <p className="text-3xl font-normal">$1250</p>
         </div>
       </div>
     </div>
