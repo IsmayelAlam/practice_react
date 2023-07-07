197a2088b222ea38dd55be36493ba5af2df5d5f6
IsmayelAlam <ismayelalam0@gmail.com>
2023-07-07 22:57:55 +0600

    add pre written files file and fix bugs

--- react_practice011/src/App.jsx
+++ react_practice011/src/App.jsx
@@ -3,14 +3,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="homepage" element={<Homepage />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ react_practice011/src/pages/AppLayout.jsx
@@ -0,0 +1,5 @@
+import style from "./style/AppLayout.module.css";
+
+export default function AppLayout() {
+  return <div className={style.app}>AppLayout</div>;
+}
