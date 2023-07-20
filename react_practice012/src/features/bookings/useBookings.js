f7af3fbe7cd85c4e9629a80a7ad90cedb1d904a3
IsmayelAlam <ismayelalam0@gmail.com>
2023-07-20 13:00:26 +0600

    add filter function for the bookings

--- react_practice012/src/features/bookings/useBookings.js
+++ react_practice012/src/features/bookings/useBookings.js
@@ -1,14 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export default function useBooking() {
+  const [searchParams] = useSearchParams();
+
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
   const {
     isLoading,
     data: Bookings,
     error,
   } = useQuery({
-    queryKey: ["Bookings"],
-    queryFn: getBookings,
+    queryKey: ["Bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, Bookings, error };
